6e662834a4944735aa618c1d9363d86aa7dc9c5e
Haofu Han <gtxx3600@gmail.com>
2018-11-19 23:55:24 +0800

    fix test deps

--- tests/test_coinselectpy.py
+++ tests/test_coinselectpy.py
@@ -8,9 +8,7 @@ import pytest
 # import os
 # sys.path.append((os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))))
 
-from click.testing import CliRunner
 from coinselectpy import coinSelect
-from coinselectpy import cli
 from coinselectpy import utils
 from coinselectpy import split
 from .coin_select_fixture import coin_select
@@ -31,17 +29,6 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'coinselectpy.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
 
 
 def test_utils():
